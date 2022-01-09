b6e327d3b6cf9a5a279441404e4bf4e973ecbc97
kaylavoogd <91560598+kaylavoogd@users.noreply.github.com>
2022-01-10 04:09:20 +0500

    Update [Bid].js

--- pages/api/nft/[Bid].js
+++ pages/api/nft/[Bid].js
@@ -12,6 +12,8 @@ export default function getNftById(req, res){
     //  all_data[0].url = "asd";
     //console.log(all_data[0]);
     //console.log(content[0]);
+    const  url = window.location.href;
+    const urlId = url.substring(url.lastIndexOf('/') + 1);
     const web3 = new Web3(speedy_nodes);
     const contract = new web3.eth.Contract(contract_abi, contract_address);
     //await Web3.givenProvider.enable()
@@ -22,13 +24,13 @@ export default function getNftById(req, res){
             if(parseInt(req.query.id) <=  parseInt(result) ){
                 res.json(content[req.query.id]);
             }else{
-                res.json(req);
-//                res.json({
-//                    'result' : result,
-//                    'requestId' : req.query.id,
-//                    'condition' : parseInt(req.query.id),
-//                    'resultCondition' : parseInt(result)
-//                });
+               res.json({
+                   'result' : result,
+                   'requestId' : req.query.id,
+                   'condition' : parseInt(req.query.id),
+                   'urlId': urlId,
+                   'resultCondition' : parseInt(result)
+               });
                //res.json("Nft Not Minted Yet!");
             }
             
