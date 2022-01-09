cb4043821733609d0e0c61b34b0938f122d96842
kaylavoogd <91560598+kaylavoogd@users.noreply.github.com>
2022-01-10 04:20:41 +0500

    Update [Bid].js

--- pages/api/nft/[Bid].js
+++ pages/api/nft/[Bid].js
@@ -28,7 +28,6 @@ export default function getNftById(req, res){
                    'result' : result,
                    'requestId' : req.query.id,
                    'condition' : parseInt(req.query.id),
-                   'urlId': url,
                    'resultCondition' : parseInt(result)
                });
                //res.json("Nft Not Minted Yet!");
