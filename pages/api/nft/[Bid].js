7ee60c97c944bc5abd3c8a898d12e1b4201bf675
kaylavoogd <91560598+kaylavoogd@users.noreply.github.com>
2022-01-10 04:00:13 +0500

    print request for minting.

--- pages/api/nft/[Bid].js
+++ pages/api/nft/[Bid].js
@@ -22,12 +22,13 @@ export default function getNftById(req, res){
             if(parseInt(req.query.id) <=  parseInt(result) ){
                 res.json(content[req.query.id]);
             }else{
-               res.json({
-                   'result' : result,
-                   'requestId' : req.query.id,
-                   'condition' : parseInt(req.query.id),
-                   'resultCondition' : parseInt(result)
-               });
+                res.json(req);
+//                res.json({
+//                    'result' : result,
+//                    'requestId' : req.query.id,
+//                    'condition' : parseInt(req.query.id),
+//                    'resultCondition' : parseInt(result)
+//                });
                //res.json("Nft Not Minted Yet!");
             }
             
